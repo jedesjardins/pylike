a1c10b3faa490c6b7024c7f1aa6741561c155da7
jedesjardins <jedesjardins@umass.edu>
2017-03-22 15:37:51 -0400

    Initial commit

---
+++ engine/actor.py
@@ -0,0 +1,9 @@
+class Actor():
+	"""Base class for objects in the game it's an Enitity with components
+	   somehow"""
+	def __init__(self):
+		print("init")
+
+	def update():
+
+	def draw():
---
+++ main.py
@@ -0,0 +1,24 @@
+import os, sys
+import pygame
+
+pygame.init()
+
+
+def main():
+	os.
+
+
+
+	size = (320, 240)
+	black = (0, 0, 0)
+	screen = pygame.display.set_mode(size)
+
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT: sys.exit()
+
+		screen.fill(black)
+		pygame.display.flip()
+
+if __name__ == '__main__':
+    main()
