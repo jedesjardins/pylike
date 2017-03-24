c6e2f4714dd4d9f362f20d5cf1b1b6ebe67c351a
jedesjardins <jedesjardins@umass.edu>
2017-03-24 01:03:20 -0400

    Interesting input handling

--- data/states/menustate.py
+++ data/states/menustate.py
@@ -1,7 +1,24 @@
 from engine.state import State
+from engine import Viewport
+import pygame
 
 class MenuState(State):
 
-	def __init__(self):
-		print("\tMenuState, init")
-		super().__init__()
+    def __init__(self):
+        # print("\tMenuState, init")
+        super().__init__()
+        self.viewport = Viewport.Viewport()
+        self.x, self.y = (0, 0)
+
+    def handle_events(self, keys):
+        if "mousemove" in keys:
+            self.x, self.y = keys["mousemove"]
+
+    def update(self, dt):
+        pass
+
+    def draw(self):
+        # self.viewport.draw_rect((10, 10, 20, 20))
+        pygame.draw.line(self.viewport.screen, (255, 255, 255), 
+            (self.x, self.y), (400, 300))
+        self.viewport.draw()
--- pylike.py
+++ pylike.py
@@ -5,7 +5,7 @@ import pygame
 from engine.engine import Engine
 from engine.state import State
 # from engine.ecs import models, managers, exceptions
-from engine import Viewport
+# from engine import Viewport
 
 from data.states import menustate
 
@@ -14,31 +14,26 @@ def main():
     pygame.init()
     
     frame_lock = pygame.time.Clock()
-    viewport = Viewport.Viewport()
     
     engine = Engine()
-    start_state = menu_state.MenuState()
+    start_state = menustate.MenuState()
     engine.push_state(start_state)
 
-    frames_per_second_max = 1
+    frames_per_second_max = 30
     dt = 1/frames_per_second_max
 
 
-    while True:
+    while engine.running:
         engine.handle_events()
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
-
         engine.update(dt)
-
-        viewport.draw_rect((10, 10, 20, 20))
-        viewport.draw()
 
         engine.draw()
 
         # TODO(jhives): rework frame time to use dt
         dt = frame_lock.tick(frames_per_second_max)
+
+    # TODO(jhives): make sure to close any states left open
 
 if __name__ == '__main__':
     main()
